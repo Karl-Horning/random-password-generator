e1c1ff8e3707e1eed44f0768e4ecbb2e8e26eb99
Karl Horning <karl.horning@learnlight.com>
2021-12-25 22:17:45 +0000

    Create generate-random-password.js
    Added functions to generate a cryptographically insecure random password

---
+++ generate-random-password.js
@@ -0,0 +1,34 @@
+const elPasswordOutput = document.getElementById('password')
+const btnGeneratePassword = document.getElementById('generatePassword')
+
+function getRandomInt(maxInt) {
+    return Math.floor(
+        Math.random() * Math.floor(maxInt)
+    )
+}
+
+function generateRandomPassword() {
+    let password = ''
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    let randomChr
+    let randomNum
+
+    for (let i = 1; i < 16; i++) {
+        if (i % 4 === 0) {
+            randomChr = '-'
+        } else {
+            randomNum = getRandomInt(characters.length)
+            randomChr = characters[randomNum]
+        }
+        password += randomChr
+    }
+
+    return password
+}
+
+function writePassword() {
+    elPasswordOutput.innerText = generateRandomPassword()
+}
+
+btnGeneratePassword.addEventListener('click', writePassword)
+document.addEventListener('DOMContentLoaded', writePassword)
